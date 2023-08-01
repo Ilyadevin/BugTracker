f9006acc07bcab6f37ff54fb194655500db159ae
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2023-08-01 14:03:32 +0300

    Some Mocks for data managment

---
+++ Data/Mocks/MockBugs.cs
@@ -0,0 +1,26 @@
+﻿using BugTracker.Data.Interfaces;
+using BugTracker.Data.Models;
+
+namespace BugTracker.Data.Mocs
+{
+    public class MockBugs : IBugs
+    {
+        private readonly IBugPriority bugPriority = new MockBugPriority();
+        public IEnumerable<Bug> Bugs { 
+            get {
+                return new List<Bug>
+                {
+                    new Bug{Name="error in smth", IsSolved=false, CreationDate=DateTime.Today.AddDays(-1), 
+                        DateOfLastInteraction = DateTime.Today, Id = Guid.NewGuid(), LongDescription="Long descr", 
+                        ShortDescription = "shortdescr", Priority =bugPriority.AllBugPriorities.First()}
+                };
+            }
+        }
+        public IEnumerable<Bug> getSolvedBugs { get; set; }
+
+        public Bug getObjectBug(Guid BugId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
