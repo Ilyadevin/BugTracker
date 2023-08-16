613d3db5df40e8e3972fc80bbffb6a23b77e48b9
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2023-08-16 13:23:16 +0300

    adding new page and controllers

---
+++ ViewModels/BugsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Data.Models;
+
+namespace BugTracker.ViewModels
+{
+    public class BugsListViewModel
+    {
+        public IEnumerable<Bugs> AllBugs {  get; set; }
+        public string CurrCategory { get; set; }
+    }
+}
