612e8a1f667de5c85ddea17d195ea02a93a1e614
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2023-08-28 14:53:17 +0300

    specifing bug model

--- Data/Models/Bugs.cs
+++ Data/Models/Bugs.cs
@@ -1,16 +1,22 @@
-﻿namespace BugTracker.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Data.Models
 {
     public class Bugs
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage ="Need to specify a name of bug")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Need to specify a short description")]
         public string ShortDescription { get; set; }
         public string? LongDescription { get; set; }
+        [Required(ErrorMessage = "Need to specify a date of bug appearance")]
         public DateTime CreationDate { get; set; }
         public DateTime? DateOfLastInteraction { get; set; }
         public string ScreenShot { get; set; }
         public bool IsSolved { get; set; }
         public Guid PriorityID { get; set; }
+        //[Required(ErrorMessage = "Need to select a bug priority")]
         public BugPriority Priority { get; set; }
 
     }
