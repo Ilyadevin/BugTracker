33226b175e702c7bd47e0165c8978d467b5213a5
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2023-08-30 16:07:45 +0300

    DBConnection & data filling example done

--- Migrations/20230830130435_initial1.cs
+++ Migrations/20230830130435_initial1.cs
@@ -3,10 +3,12 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace BugTracker.Migrations
 {
     /// <inheritdoc />
-    public partial class initial : Migration
+    public partial class initial1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -15,10 +17,10 @@ namespace BugTracker.Migrations
                 name: "BugPriority",
                 columns: table => new
                 {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    BugClassName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    LongDescription = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BugClassName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -35,9 +37,9 @@ namespace BugTracker.Migrations
                     LongDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
                     DateOfLastInteraction = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    ScreenShot = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ScreenShot = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     IsSolved = table.Column<bool>(type: "bit", nullable: false),
-                    PriorityID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                    PriorityID = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -48,6 +50,26 @@ namespace BugTracker.Migrations
                         principalTable: "BugPriority",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "BugPriority",
+                columns: new[] { "Id", "BugClassName", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Low", "Description" },
+                    { 2, "Medium", "Description" },
+                    { 3, "High", "Description" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Bugs",
+                columns: new[] { "Id", "CreationDate", "DateOfLastInteraction", "IsSolved", "LongDescription", "Name", "PriorityID", "ScreenShot", "ShortDescription" },
+                values: new object[,]
+                {
+                    { new Guid("4302e22f-7871-45fc-a920-0d8fab157a9f"), new DateTime(2023, 8, 29, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 8, 30, 0, 0, 0, 0, DateTimeKind.Local), false, "Long descr", "error in smth, middle priority", 2, "/img/34a012f9-68cf-4e21-81b2-2023d75c4f96.png", "shortdescr" },
+                    { new Guid("951e0073-b171-4cf7-a217-08f9fe128b7e"), new DateTime(2023, 8, 29, 0, 0, 0, 0, DateTimeKind.Local), null, false, "Long descr", "error in smth, low proirity", 1, "/img/34a012f9-68cf-4e21-81b2-2023d75c4f96.png", "shortdescr" },
+                    { new Guid("b759ed5c-24e3-42fe-b2be-1ffc48b2d50d"), new DateTime(2023, 8, 29, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2023, 8, 30, 0, 0, 0, 0, DateTimeKind.Local), false, "Long descr", "error in smth, high priority", 3, "/img/34a012f9-68cf-4e21-81b2-2023d75c4f96.png", "shortdescr" }
                 });
 
             migrationBuilder.CreateIndex(
