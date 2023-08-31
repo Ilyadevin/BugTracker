b834704982a8723c2de130b3d706fea716e72284
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2023-08-31 18:57:58 +0300

    upd view & new butn to creation page

---
+++ Data/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Data.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Roles role { get; set; }
+    }
+}
