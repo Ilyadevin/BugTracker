6167c3a0555175ecba0b368e3ab3e7ff9ab8459f
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2023-10-11 13:57:56 +0300

    old upd prepare for some changes

---
+++ Controllers/DetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Controllers
+{
+    public class DetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
