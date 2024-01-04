e59d5890205537cccf8339258c5908fcdf2372d9
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-01-04 16:44:59 +0300

    models final version

---
+++ Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string? ProfileImageUrl { get; set; }
+        public ICollection<Bug> Bugs { get; set; }
+    }
+}
