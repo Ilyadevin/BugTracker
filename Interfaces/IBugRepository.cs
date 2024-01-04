921d7d58a96eee5407a54af8a28bd0301e903d4a
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-01-04 16:49:28 +0300

    checkpoint for interfaces

---
+++ Interfaces/IBugRepository.cs
@@ -0,0 +1,29 @@
+﻿using BugTracker.Data.Enum;
+using BugTracker.Models;
+
+namespace BugTracker.Interfaces
+{
+    public interface IBugRepository
+    {
+        Task<IEnumerable<Bug>> GetAll();
+
+
+        Task<IEnumerable<Bug>> GetBugsByPriorityAndSliceAsync(BugPriority priority, int offset, int size);
+
+        Task<Bug?> GetByIdAsync(Guid id);
+
+        Task<Bug?> GetByIdAsyncNoTracking(string id);
+
+        Task<int> GetCountAsync();
+
+        Task<int> GetCountByPriorityAsync(BugPriority priorityy);
+
+        bool Add(Bug club);
+
+        bool Update(Bug club);
+
+        bool Delete(Bug club);
+
+        bool Save();
+    }
+}
